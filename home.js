8ef1e0334da16a746fa031a1c87e811d37a3686d
Syful-Islam1 <scandpp@gmail.com>
2026-07-03 00:30:33 +0600

    add money functionality done

---
+++ home.js
@@ -0,0 +1,15 @@
+document.getElementById("add-money-btn").addEventListener("click", function(e){
+    e.preventDefault()
+    const bank = document.getElementById("bank").value
+    const accountNumber = document.getElementById("account-number").value
+    const amount = parseInt(document.getElementById("add-amount").value)
+    const pin = document.getElementById("add-pin").value
+
+    const avilableBalance = parseInt(document.getElementById("avilable-balance").innerText)
+    console.log(avilableBalance)
+
+    const totalNewAvilableBalance = amount+avilableBalance
+
+    document.getElementById("avilable-balance").innerText = totalNewAvilableBalance
+
+})
